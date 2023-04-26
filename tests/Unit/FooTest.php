e84cc6341a9d504883236868141df8e3d427a882
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-26 10:41:14 -0500

    Update FooTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/FooTest.php
+++ tests/Unit/FooTest.php
@@ -5,18 +5,12 @@ declare(strict_types=1);
 namespace Ghostwriter\wip\Tests\Unit;
 
 use Ghostwriter\wip\Foo;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
-/**
- * @coversDefaultClass \Ghostwriter\wip\Foo
- *
- * @internal
- *
- * @small
- */
+#[CoversClass(Foo::class)]
 final class FooTest extends TestCase
 {
-    /** @covers \Ghostwriter\wip\Foo::test */
     public function test(): void
     {
         self::assertTrue((new Foo())->test());
