7a2860e92c73f3f50797691e5f4e22fd199a0a55
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-08 13:48:34 -0500

    Update FooTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/FooTest.php
+++ tests/Unit/FooTest.php
@@ -4,15 +4,31 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Wip\Tests\Unit;
 
+use Generator;
 use Ghostwriter\Wip\Foo;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(Foo::class)]
 final class FooTest extends TestCase
 {
-    public function test(): void
+    /**
+     * @return Generator<array{bool}>
+     */
+    public static function dataProvider(): Generator
     {
+        yield from [
+            'true' => [true],
+            'false' => [false],
+        ];
+    }
+
+    #[DataProvider('dataProvider')]
+    public function test(bool $value): void
+    {
+        self::assertSame($value, $value);
+
         self::assertTrue((new Foo())->test());
     }
 }
