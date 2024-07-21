44a8a84273f50c03b59a1ed64257f9300f28f422
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-21 01:12:26 -0500

    Update Foo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Foo.php
+++ src/Foo.php
@@ -4,11 +4,22 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Wip;
 
+use Ghostwriter\Wip\Interface\FooInterface;
+
 /** @see FooTest */
-final class Foo
+final class Foo implements FooInterface
 {
+    public function __construct()
+    {
+    }
+
     public function test(): bool
     {
         return true;
+    }
+
+    public static function new(): self
+    {
+        return new self();
     }
 }
