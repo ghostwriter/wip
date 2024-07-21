45526e6f4a5312835c502612c51d84cdb404c0d0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-21 01:12:26 -0500

    Update console.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- bin/console.php
+++ bin/console.php
@@ -5,23 +5,33 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Wip\Console;
 
+use ErrorException;
 use Ghostwriter\Wip\Foo;
 
+use const DIRECTORY_SEPARATOR;
 use const STDERR;
 
+use function dirname;
+use function file_exists;
 use function fwrite;
-use function sprintf;
+use function set_error_handler;
 
 /** @var ?string $_composer_autoload_path */
-(static function (string $composerAutoloadPath): void {
-    /** @psalm-suppress UnresolvableInclude */
-    require $composerAutoloadPath ?: fwrite(
-        STDERR,
-        sprintf('[ERROR]Cannot locate "%s"\n please run "composer install"\n', $composerAutoloadPath)
-    ) && exit(1);
+(static function (string $autoloader): void {
+    set_error_handler(static function (int $severity, string $message, string $file, int $line): never {
+        throw new ErrorException($message, 255, $severity, $file, $line);
+    });
+
+    if (! file_exists($autoloader)) {
+        $message = '[ERROR]Cannot locate "' . $autoloader . '"\n please run "composer install"\n';
+        fwrite(STDERR, $message);
+        exit;
+    }
+
+    require $autoloader;
 
     /**
      * #BlackLivesMatter.
      */
-    echo (new Foo())->test();
-})($_composer_autoload_path ?? null);
+    echo Foo::new()->test();
+})($_composer_autoload_path ?? dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php');
