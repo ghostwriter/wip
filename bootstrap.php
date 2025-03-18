6849d8ebebe41da614f914199b9d7ac58629a782
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-18 18:56:14 -0500

    Update `bootstrap.php` file (#158)
    Updates `bootstrap.php` file.
    
    - Update `bootstrap.php`
    
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- bootstrap.php
+++ bootstrap.php
@@ -12,11 +12,11 @@ if (! $classLoader instanceof ClassLoader) {
     throw new \RuntimeException('Class loader not found');
 }
 
-$FixturePath = __DIR__ . \DIRECTORY_SEPARATOR . 'Fixture';
+$path = __DIR__ . \DIRECTORY_SEPARATOR . 'Fixture';
 
-if (\is_dir($FixturePath)) {
+if (\is_dir($path)) {
     /** @psalm-suppress UncaughtThrowInGlobalScope */
-    $classLoader->addPsr4('Tests\Fixture\', $FixturePath);
+    $classLoader->addPsr4('Tests\Fixture\', $path);
 }
 
 return $classLoader;
